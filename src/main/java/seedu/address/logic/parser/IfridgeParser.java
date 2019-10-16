a5d56b4f1a1cc35d3b6e23d2219fba76a0be6e0a
kelvinnharris <kelvinnharris@gmail.com>
2019-10-16 19:54:09 +0800

    Add 'use' functionality for grocery list

--- src/main/java/seedu/address/logic/parser/IfridgeParser.java
+++ src/main/java/seedu/address/logic/parser/IfridgeParser.java
@@ -20,7 +20,7 @@ public class IfridgeParser {
     /**
      * Used for initial separation of command word and args.
      */
-    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<listType>\\S+)(?<others>.*)");
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<listType>\\S+)(?<listCommand>.*)");
 
     /**
      * Parses user input into command for execution.
@@ -36,12 +36,13 @@ public class IfridgeParser {
         }
 
         final String listType = matcher.group("listType");
-        final String others = matcher.group("others");
+        final String listCommand = matcher.group("listCommand");
         switch (listType) {
         case GroceryListParser.LIST_TYPE_WORD:
-            return new GroceryListParser().parseCommand(others);
+            return new GroceryListParser().parseCommand(listCommand);
+
         case TemplateListParser.LIST_TYPE_WORD:
-            return new TemplateListParser().parseCommand(others);
+            return new TemplateListParser().parseCommand(listCommand);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
