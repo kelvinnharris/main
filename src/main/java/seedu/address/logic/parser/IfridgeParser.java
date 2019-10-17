6646b7a5b4d09b6dfce20a18eb69a01e334f4748
kelvinnharris <kelvinnharris@gmail.com>
2019-10-17 10:00:11 +0800

    Rename parser.groceryList to parser.grocerylist

--- src/main/java/seedu/address/logic/parser/IfridgeParser.java
+++ src/main/java/seedu/address/logic/parser/IfridgeParser.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.logic.parser.groceryList.GroceryListParser;
+import seedu.address.logic.parser.grocerylist.GroceryListParser;
 import seedu.address.logic.parser.templateList.TemplateListParser;
 
 /**
